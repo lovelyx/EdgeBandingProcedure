b151911cca36b0e9f7d3fc9ed8afae57ff8441c3
yixiaoliu <2579196673@qq.com>
2023-11-20 09:35:43 +0800

    算法优化,bug修复,新增数据库设置，mpr处理

---
+++ afterEnd/MprProcessing.py
@@ -0,0 +1,37 @@
+import os
+import pathlib
+import logging
+import traceback
+import pymsgbox as mb
+
+
+def main(MprInput,MprOutput):
+    try:
+        for root, dirs, files in os.walk(MprInput):
+            for i in files:
+                path = os.path.join(root, i)
+                with open(path) as fr:
+                    filepatch = MprOutput + "/" + path.split('\\')[-2] +"/" +path.split('\\')[-1]
+                    filepatch2 = MprOutput + "/" + path.split('\\')[-2]
+                    pathlib.Path(filepatch2).mkdir(parents=True, exist_ok=True)
+                    with open(filepatch, 'w') as fw:
+                        # 循环读取文件内容，逐行修改
+                        for line in fr:
+                            if line.startswith('<131 \\UfluBohr\\'):
+                                line = line.replace('<131 \\UfluBohr\\','<102\\ Vertical Drilling\\')
+                            fw.write(line)
+
+
+        return True
+
+    except:
+        logging.basicConfig(filename='log.txt', level=logging.DEBUG,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+        # 方案一，自己定义一个文件，自己把错误堆栈信息写入文件。
+        errorFile = open('log.txt', 'a')
+        errorFile.write(traceback.format_exc())
+        errorFile.close()
+        # yield f"{j}文件处理失败，请联系管理员"
+        # tkinter.messagebox.showerror(title='错误',message=f"{j.split('.')[0]}文件处理失败，请联系管理员")
+        mb.alert(f"mpr文件处理失败，请联系管理员", '报错')
+        # win32api.MessageBox(0,f"{j.split('.')[0]}文件处理失败，请联系管理员","错误",win32con.MB_OK)
